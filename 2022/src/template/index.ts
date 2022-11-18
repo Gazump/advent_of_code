a70edb101d8e429496d77b992867325338ed07a9
Nathan Boshoff <dc.shephard@gmail.com>
2022-11-18 11:43:24 +0000

    setup: 2022

---
+++ 2022/src/template/index.ts
@@ -0,0 +1,42 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const output = 0;
+
+  return output.toString();
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const output = 0;
+
+  return output.toString();
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
