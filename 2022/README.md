d849d1efd5f22e6b7adfb153cf4c06facbeb9bc1
Nathan Boshoff <dc.shephard@gmail.com>
2022-12-03 11:18:19 +0000

    solution: 2022 day 3

--- 2022/README.md
+++ 2022/README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -56,16 +56,16 @@ Both parts: 0.9160999999999999ms
 
 ```
 Day 02
-Time part 1: 1.367ms
-Time part 2: 1.387ms
-Both parts: 2.7539ms
+Time part 1: 1.357ms
+Time part 2: 1.368ms
+Both parts: 2.7248ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.381ms
+Time part 2: 12.764ms
+Both parts: 13.1449ms
 ```
 
 ```
@@ -223,8 +223,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 3.67ms
+Total stars: 6/50
+Total time: 16.786ms
 ```
 
 <!--/RESULTS-->
---
+++ 2022/src/day03/index.ts
@@ -0,0 +1,86 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const getCommonCharacter = (a: string, b: string, c?: string): string | undefined => {
+  return Array.from(a).find((char) => {
+    return c && b.includes(char) ? c.includes(char) : b.includes(char);
+  });
+}
+
+const getPriority = (char: string): number => {  
+  const code = char.charCodeAt(0);
+  return  code >= 97 ? code - 96 : code - 38;
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput).split("\n");
+  let sum = 0;
+
+  input.forEach((line) => {
+    const compartments = { one:line.slice(0, line.length / 2), two: line.slice(line.length / 2)};    
+    const error = getCommonCharacter(compartments.one, compartments.two);    
+    const priority = getPriority(error!);    
+    sum += priority;    
+  });
+
+  return sum.toString();
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput).split("\n");
+  let sum = 0;
+
+  for (let i = 0; i < input.length; i += 3) {
+    const compartments = { one: input[i], two: input[i+1], three: input[i+2] };
+    const error = getCommonCharacter(compartments.one, compartments.two, compartments.three);
+    sum += getPriority(error!);
+  }
+  
+  return sum.toString();
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+          vJrwpWtwJgWrhcsFMMfFFhFp
+          jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+          PmmdzqPrVvPwwTWBwg
+          wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+          ttgJtRGJQctTZtZT
+          CrZsJsPPZsGzwwsLwLmpwMDw
+        `,
+        expected: "157",
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+          vJrwpWtwJgWrhcsFMMfFFhFp
+          jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+          PmmdzqPrVvPwwTWBwg
+          wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+          ttgJtRGJQctTZtZT
+          CrZsJsPPZsGzwwsLwLmpwMDw
+        `,
+        expected: "70",
+      },
+      {
+        input: `
+          NGWdQgDDHGJgQLznzzsJFFzvzB
+          twRCpZVjVWqVSqVwwjtZfrrfntfvznBssBncfLrc
+          jRRwCqwCZhlhZRpSZpjSqWwqmDMQdMmHPQQMHGdlHdTldNGd
+        `,
+        expected: "49",
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
